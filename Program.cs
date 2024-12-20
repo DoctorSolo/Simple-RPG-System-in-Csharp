0b12a9501f9cbd5ad760035b321c249e2b9f2823
Miguel Eduardo <dasilvamigueleduardo@gmail.com>
2024-12-19 22:51:15 -0300

    test2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,5 @@
 ﻿using RPG_system.Entities.EntitieCreationAtribute;
 
-//Races.Human Alice = new Races.Human(60, 0);
-//new Clase().Warrior(Alice);
-
-//Alice.Show();
-
-//Alice.NextLevel(1);
-
-//Alice.Show();
 
 EntitieCreationAtribute Alice = new EntitieCreationAtribute(60.00M, 100, 80, 10, 5, 40, 10, 10, 60, 0.8M, 10, 30, 10, 10);
 
--- obj/Debug/net8.0/RPG system.AssemblyInfo.cs
+++ obj/Debug/net8.0/RPG system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a81980bb91bf1fb6c80d7426736b29c2986913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f36b259e7efdadd11a1f7319362ce3303521d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
