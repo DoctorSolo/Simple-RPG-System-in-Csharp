57da8b255355e58a7daa38c64f166f0e62a774ed
Miguel Eduardo <dasilvamigueleduardo@gmail.com>
2025-01-01 12:15:14 -0300

    feat(ClassSystem): Add class in RPG system

--- Entitie/ClassSystem.cs
+++ Entitie/ClassSystem.cs
@@ -1,4 +1,9 @@
-﻿namespace RPG_system.Entitie.ClassSystem
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Threading.Channels;
+
+namespace RPG_system.Entitie.ClassSystem
 {
 	/*
 	 * @DOCTOR SOLO
@@ -12,9 +17,22 @@
 	{
 		/* 
 		 * Correct order: 
-		 * [Weight, Life, Histamine, Mana, Inventor, PhisicalDefense, MagicalDefense, AttSpeed, Strenght, CriticalChance,
-		 * CriticalDamage, MovSpeed, MagicProficience, MagicalAttk]
+		 * [Level, Weight, Life, Histamine, Mana, Inventor, Phisical Defense, Phisical Damage, Magical Damage, Magical Defense, Att Speed,
+		 * Strenght, Critical Chance, Critical Damage, Mov Speed, Magic Proficience, Magical Att];
+		 * 
+		 * distribution:
+		 * 120 pt
 		*/
-		public static decimal[] Warrior = [1.0M, 1.0M, 1.20M, 1.20M, 1.0M, 1.0M, 1.20M, 1.20M, 1.0M, 1.0M, 1.1M, 1.30M, 1.05M, 1.1M, 1.01M, 1.0M, 1.0M];
+		public static decimal[] Warrior = [/*Level*/1.0M,/*Weight*/1.0M,/*Life*/1.3M,/*Histamine*/1.3M,/*Mana*/0.9M,/*Inventor*/1.0M,
+			/*PhisicalDefense*/1.2M,/*PhisicalDamage*/1.0M,/*MagicalDamage*/1.0M,/*MagicalDefense*/0.9M,/*Att Speed*/1.0M,/*Strenght*/1.4M,
+			/*CriticalChance*/1.0M,/*CriticalDamage*/1.4M,/*MovSpeed*/1.0M,/*MagicProficience*/0.9M,/*MagicalAtt*/0.9M];
+
+		public static decimal[] Wizard = [/*Level*/1.0M,/*Weight*/1.0M,/*Life*/1.0M,/*Histamine*/0.9M,/*Mana*/1.3M,/*Inventor*/1.3M,
+			/*PhisicalDefense*/0.1M,/*PhisicalDamage*/1.0M,/*MagicalDamage*/1.0M,/*MagicalDefense*/1.3M,/*Att Speed*/1.0M,/*Strenght*/0.8M,
+			/*CriticalChance*/1.0M,/*CriticalDamage*/1.0M,/*MovSpeed*/1.0M,/*MagicProficience*/1.4M,/*MagicalAtt*/1.2M];
+
+		public static decimal[] Assassin = [/*Level*/1.0M,/*Weight*/1.0M,/*Life*/0.8M,/*Histamine*/1.1M,/*Mana*/1.1M,/*Inventor*/1.1M,
+			/*PhisicalDefense*/0.8M,/*PhisicalDamage*/1.0M,/*MagicalDamage*/1.0M,/*MagicalDefense*/0.8M,/*Att Speed*/1.5M,/*Strenght*/1.2M,
+			/*CriticalChance*/1.0M,/*CriticalDamage*/1.4M,/*MovSpeed*/1.0M,/*MagicProficience*/1.2M,/*MagicalAtt*/1.2M];
 	}
 }
